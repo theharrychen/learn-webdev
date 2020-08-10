452af233713d0f6b51b6c8a07fa3e7774da2a411
Harry Chen <harry.chen1@ucalgary.ca>
2020-08-09 22:12:31 -0600

    Add hashing and salting passwords

--- secrets-app/app.js
+++ secrets-app/app.js
@@ -2,12 +2,30 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.DB_URL || "mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plug;
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -19,6 +37,42 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  bcrypt.hash(req.body.password, saltRounds, (error, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+  
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, (error, result) => {
+          if (result) {
+            res.render("secrets");
+          }
+        });
+      }
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
